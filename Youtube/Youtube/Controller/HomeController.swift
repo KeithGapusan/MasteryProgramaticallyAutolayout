c2288ec90a9501c6f6b2b917ecf6abbd384e0cb5
Keith Randell Gapusan <Keith@Keiths-MacBook-Pro.local>
2018-12-06 08:55:41 +0800

    Update: implement the menubar cell and BaseCell to override collectioncells

--- Youtube/Youtube/Controller/HomeController.swift
+++ Youtube/Youtube/Controller/HomeController.swift
@@ -24,10 +24,24 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         collectionView.register(VideoCollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView.backgroundColor = UIColor.white
+        collectionView.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
+        collectionView.scrollIndicatorInsets = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
+        
+        setupMenuBar()
+        
+    }
+    let menuBar:MenuBar = {
+        let mb =  MenuBar()
+        return mb
+    }()
+    private func setupMenuBar(){
+        view.addSubview(menuBar)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: menuBar)
+        view.addConstraintsWithFormat(format: "V:|[v0(50)]|", views: menuBar)
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
-    }
+    } 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
      //   cell.backgroundColor = .red
