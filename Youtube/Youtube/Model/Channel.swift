761ee2fbee2042ed949dfe9bd4b8cc6501d2cde1
Keith Randell Gapusan <keithrandellgapusan@Keiths-Macbook.local>
2018-12-23 10:28:06 +0800

    Fixed: Added untracked files - Channel model
    -q
    wq

---
+++ Youtube/Youtube/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Youtube
+//
+//  Created by Keith Randell Gapusan on 18/12/2018.
+//  Copyright © 2018 Keith Randell Gapusan. All rights reserved.
+//
+
+import UIKit
+
+class Channel: NSObject {
+    var name: String?
+    var profileImageName : String?
+    
+}
