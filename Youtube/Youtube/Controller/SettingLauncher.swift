8acc37f89b8c9d9712ad9ca14190abbd2716fc9f
keith31 <ksgapusan@straightarrow.com.ph>
2018-12-25 23:00:47 +0800

    Added SettingLauncher Controller

---
+++ Youtube/Youtube/Controller/SettingLauncher.swift
@@ -0,0 +1,9 @@
+//
+//  SettingLauncher.swift
+//  Youtube
+//
+//  Created by Keith Randell Gapusan on 25/12/2018.
+//  Copyright © 2018 Keith Randell Gapusan. All rights reserved.
+//
+
+import Foundation
