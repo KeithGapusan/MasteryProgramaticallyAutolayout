f7b4089ad09c844238e1c9e77a14eb9b80aeec68
Keith Randell Gapusan <keithrandellgapusan@keiths-macbook.local>
2019-01-04 17:58:09 +0800

    Update: added a fade-in and fade-out extension of UIView

--- Youtube/Youtube/Helpers/Extensions.swift
+++ Youtube/Youtube/Helpers/Extensions.swift
@@ -25,6 +25,21 @@ extension UIView{
         }
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
     }
+    
+    func fadeIn(duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)
+    }
+    
+    func fadeOut(duration: TimeInterval = 1.0, delay: TimeInterval = 3.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.alpha = 0.0
+        }, completion: completion)
+
+    }
+    
+
 }
 let imageCache = NSCache<AnyObject, AnyObject>()
 
